979df11c1fff4108173148ec11dbc86c776ad6c7
laure leter <laure.leter@gmail.com>
2022-01-06 11:16:36 +0100

    first request & graphql api connection

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
 
 import Head from 'next/head'
 import { Categories, PostCard, PostWidget } from '../components' 
+import {getPosts} from '../services'
 
-
-const posts = [
-  {title: 'React Testing', excerpt: 'Learn React Testing'},
-  {title: 'React WITH TAILWIND', excerpt: 'Learn Tailwind'}
-]
-
-export default function Home() {
+export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +12,7 @@ export default function Home() {
       </Head>
       <div className ="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-        {posts.map((post) => (<PostCard post ={post} key={post.title} />
+        {posts.map((post, index) => (<PostCard post ={post.node} key={index} />
         ))}
       </div>
   
@@ -31,4 +26,11 @@ export default function Home() {
     </div>
   </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: {posts}
+  }
 }
