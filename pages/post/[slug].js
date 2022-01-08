5bd49a4e04c93c3b0821bcfc9fd87e0e58a575ee
laure leter <laure.leter@gmail.com>
2022-01-08 14:58:54 +0100

    comments on artciles

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import { PostDetail, Categories, PostWidget, Loader } from '../../components';
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
 
 
@@ -18,6 +18,9 @@ const PostDetails = ({ post }) => {
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
+            <Author author={post.author} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
             
           </div>
           <div className="col-span-1 lg:col-span-4">
