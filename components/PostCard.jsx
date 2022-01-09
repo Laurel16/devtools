2230224881aad4c9b758d69c843e8382539bb42e
laure leter <laure.leter@gmail.com>
2022-01-10 00:33:08 +0100

    add rich-text-react-renderer for links in rich-text graphql

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import Link from 'next/link';
 
 function PostCard({post}) {
+
+    console.log(post)
+    
+  
     return (
             <div>
             <a href={`/post/${post.slug}`}>
-            <div className="rounded overflow-hidden shadow-lg post-card h-full">
+            <div className="rounded overflow-hidden shadow-lg post-card h-full mt-10">
                 <img className="w-full" src={post.image.url} alt={post.title}/>
                 <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{post.title}</div>
                 <p className="text-gray-700 text-base min-h-[100]">
                 {post.excerpt}
+                </p>
+                <p className="text-gray-700 text-base min-h-[100]">
+                {post.categories.map(category => <div>{category.name}</div>)}
                 </p>
                 </div>
                 <div className="px-6 pt-4 pb-2">
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,45 +1,17 @@
 import React from 'react';
 import moment from 'moment';
+import { RichText } from '@graphcms/rich-text-react-renderer';
 
 const PostDetail = ({ post }) => {
-  const getContentFragment = (index, text, obj, type) => {
-    let modifiedText = text;
+    const test = post.content.raw.children
+    const test2 = test.children
+    console.log("voici")
 
-    if (obj) {
-      if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
-      }
+  
 
-      if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
-      }
+const content = post.content.raw
 
-      if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
-      }
-    }
-
-    switch (type) {
-      case 'heading-three':
-        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-      case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-      case 'heading-four':
-        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
-      case 'image':
-        return (
-          <img
-            key={index}
-            alt={obj.title}
-            height={obj.height}
-            width={obj.width}
-            src={obj.src}
-          />
-        );
-      default:
-        return modifiedText;
-    }
-  };
+console.log(content)
 
   return (
     <>
@@ -55,9 +27,8 @@ const PostDetail = ({ post }) => {
                 height="30px"
                 width="30px"
                 className="align-middle rounded-full"
-                src={post.author.photo}
+                src={post.author.photo.url}
               />
-              <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
             </div>
             <div className="font-medium text-gray-700">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -67,11 +38,34 @@ const PostDetail = ({ post }) => {
             </div>
           </div>
           <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-          {post.content.raw.children.map((typeObj, index) => {
-            const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+          <RichText
+      content={content}
+      renderers={{
+        h3: ({ children }) => <h3 className="text-xl font-semibold mb-4">{children}</h3>,
+        h4: ({ children }) => <h4 className="text-md font-semibold mb-4">{children}</h4>,
+        a: ({ children, openInNewTab, href, rel, ...rest }) => {
+          if (href.match(/^https?:\/\/|^\/\//i)) {
+            return (
+              <a
+                href={href}
+                target={openInNewTab ? '_blank' : '_self'}
+                rel={rel || 'noopener noreferrer'}
+                className='text-red-700'
+                {...rest}
+              >
+                {children}
+              </a>
+            );
+          }
 
-            return getContentFragment(index, children, typeObj, typeObj.type);
-          })}
+          return (
+            <Link href={href}>
+              <a {...rest}>{children}</a>
+            </Link>
+          );
+        },
+      }}
+    />
         </div>
       </div>
 
